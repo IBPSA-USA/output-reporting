14cc655160a08e252cd91e0e92c433ccf6c02ae3
Neal Kruis <neal.kruis@bigladdersoftware.com>
2026-08-12 22:19:09 -0600

    Fix dodo.py's Lattice attribute references.

--- dodo.py
+++ dodo.py
@@ -13,7 +13,7 @@ def task_validate_example_files():
     """Validates the example files against the JSON schema (and other validation steps)"""
     return {
         "file_dep": reporting_data_model.examples
-        + [schema.file_path for schema in reporting_data_model.schemas],
+        + [schema.schema.file_path for schema in reporting_data_model.schema_info],
         "actions": [(reporting_data_model.validate_example_files, [])],
     }
 
@@ -21,7 +21,7 @@ def task_validate_example_files():
 def task_generate_web_docs():
     """Generates Markdown Documentation"""
     return {
-        "file_dep": [schema.file_path for schema in reporting_data_model.schemas]
+        "file_dep": [schema.schema.file_path for schema in reporting_data_model.schema_info]
         + [template.path for template in reporting_data_model.doc_templates],
         "targets": [os.path.join(reporting_data_model.web_docs_directory_path, "public")],
         "actions": [(reporting_data_model.generate_web_documentation, [])],
